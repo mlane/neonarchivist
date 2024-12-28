4102182242f9548c75083ff2975e895528d392b2
mlane <mlane@travelpassgroup.com>
2024-12-28 09:14:27 -0700

    CHI-221-part-1 [Design] Apply Global Theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,23 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'media',
   theme: {
     extend: {
-      /** @todo add colors */
-      colors: {},
+      colors: {
+        darkBackground: '#121212',
+        darkText: '#E0E0E0',
+        lightBackground: '#FFFFFF',
+        lightText: '#2D2D2D',
+        neonGreen: '#00FF99',
+        neonPink: '#FF00FF',
+        neonCyan: '#00FFFF',
+        neonOrange: '#FF4500',
+      },
       fontFamily: {
+        firaCode: ['Fira Code', 'monospace'],
         orbitron: ['Orbitron', 'sans-serif'],
-        sans: ['Roboto', 'sans-serif'],
-        'fira-code': ['Fira Code', 'monospace'],
+        roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
